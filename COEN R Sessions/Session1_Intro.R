7b70f540509a3f47c908a2815cf48ebbeaf3976b
Nicole Harty <nharty@routt.gov>
2021-03-06 19:44:59 -0700

    Initial commit

---
+++ COEN R Sessions/Session1_Intro.R
@@ -0,0 +1,46 @@
+#COEN R and RMarkdown Professional Development Series
+#Session 1: Introduction to R and RStudio for Evaluators
+
+#Fist Authored: 2/13/2021 Nicole Harty
+#Last Updated: 3/06/2021 Nicole Harty
+
+# Session Objectives:
+## Explain file directories, file types, and project structure
+## Install and use major packages to import data and create data frames
+
+#load packages
+library(openxlsx)
+library(googlesheets4)
+library(tidyverse)
+
+#check working directory
+getwd()
+#Change wd if needed
+#first with code
+setwd("U:/COEN R PD/COEN R Sessions")
+#second using GUI
+
+#load data file
+#from .xslx file
+read.xlsx("R Workshop (Responses).xlsx")
+#from .csv file
+read.csv("R Workshop (Responses) - Form Responses 1.csv", header = TRUE)
+#save this data as a dataframe
+SurveyCSV <- read.csv("R Workshop (Responses) - Form Responses 1.csv", header = TRUE)
+SurveyXLSX <- read.xlsx("R Workshop (Responses).xlsx")
+
+
+#from API connection to Google Sheets
+read_sheet("https://docs.google.com/spreadsheets/d/17uyz1A90D6XgGKi_Qer-ssgkWRdPmKACEOHzk0FhjiI/edit#gid=790590919")
+#save this data as a dataframe
+SurveyGSheetAPI <- read_sheet("https://docs.google.com/spreadsheets/d/17uyz1A90D6XgGKi_Qer-ssgkWRdPmKACEOHzk0FhjiI/edit#gid=790590919")
+
+#look at the data
+##use data viewer
+##in console
+SurveyXLSX
+##get info about data
+colnames(SurveyXLSX)
+data.class(SurveyXLSX$`What.session(s).are.you.attending?.(Check.all.that.apply)`)
+data.class(SurveyXLSX$Timestamp)
+
---
+++ COEN R Sessions/Session2.R
@@ -0,0 +1,126 @@
+#COEN R and RMarkdown Professional Development Series
+#Session 2: Data Manipulation and Introduction to the tidyverse
+
+#Fist Authored: 2/13/2021 Nicole Harty
+#Last Updated: 2/13/2021 Nicole Harty
+
+#Session Objectives
+## Explain the purpose and function of key tidyverse packages (dplyr, lubridate, stringr, forcats, ggplot2)
+## Author code to manipulate data using the pipe operator
+## Generate summary statistics and aggregations using the tidyverse
+## Perform common factor manipulations (reordering, collapsing factors) and work with strings (trucating, matching)
+## Create new variables using criteria in the data using case_when and if_else
+
+
+#Key tidyverse packages
+##dplyr - it all starts here! data manipulation and wrangling; create new columns/variables, select columns, filter data, aggregations
+  ##dplyr functions: SELECT = choose columns, FILTER = choose rows based upon criteria, MUTATE = create new column, SUMMARISE = aggregation
+##lubridate - working with dates is tricky. lubridate makes it easier. work with date parts, durations, and intervals; calculate with dates
+##stringr - manipulate string variables; identify substrings, parse a string, etc
+##forcats - factors/categorical variables; coerce strings to factors, relevel or rename factors, collapse factors, etc
+##ggplot2 - grammar of graphics for data visualization; start with a basic plot and add detail and layers
+
+
+#First, load packages and dataset/s
+library(openxlsx)
+library(googlesheets4)
+library(tidyverse)
+library(lubridate)
+
+SurveyXLSX <- read.xlsx("R Workshop (Responses).xlsx")
+SurveyGSheetAPI <- read_sheet("https://docs.google.com/spreadsheets/d/17uyz1A90D6XgGKi_Qer-ssgkWRdPmKACEOHzk0FhjiI/edit#gid=790590919")
+
+
+#Clean up the data frame - more usable column names, set data types
+##change datetime column to be datetime
+##2 ways to do the same thing
+SurveyXLSX$Timestamp <- as.Date.numeric(SurveyXLSX$Timestamp, origin = "1899-12-30") #this gives just the DATE
+SurveyXLSX$Timestamp <- as.POSIXct(SurveyXLSX$Timestamp) #Now we get the full timestamp
+
+SurveyXLSX[,c(1)] <- as.Date.numeric(SurveyXLSX[,c(1)], origin = "1899-12-30")
+SurveyXLSX[,c(1)] <- as.POSIXct(SurveyXLSX[,c(1)])
+
+
+#the PIPE operator
+#read the pipe as "AND THEN": "Take the SurveyXLSX data frame AND THEN select a few columns AND THEN filter to rows where column __ is ___"
+SurveyXLSX %>%
+  select(Timestamp) %>%
+  filter(Timestamp < "2021-03-15")
+
+#alternative, base R, no PIPE: read inside to outside parenthesis - UCK!
+filter(select(SurveyXLSX, Timestamp), Timestamp < "2021-03-15")
+
+##Change column names using RENAME function from DPLYR package (use Help window to look up function arguments)
+SurveyXLSX <- SurveyXLSX %>%
+  rename(SessionsAttend = `What.session(s).are.you.attending?.(Check.all.that.apply)`,
+         ExperienceProgCode = `What.is.your.experience.with.programming/coding.(such.as.SQL,.SAS,.VBA,.or.complex.excel.functions)?`,
+         ExperienceR = `What.is.your.level.of.experience.with.R?`,
+         ExperienceDataNumeric = `What.is.your.level.of.experience.working.with.the.following.data.types?.[Numeric]`,
+         ExperienceDataCategorical = `What.is.your.level.of.experience.working.with.the.following.data.types?.[Categorical]`,
+         ExperienceDataFactor = `What.is.your.level.of.experience.working.with.the.following.data.types?.[Factor]`,
+         ## Type in additional columns in real time for demonstration##
+         )
+
+
+#Data Aggregations
+##How many total responses?
+SurveyXLSX %>%
+  count()
+##Save the number as a VALUE
+TotalResponses <- SurveyXLSX %>%
+  count()
+
+##How many responses each day?
+SurveyXLSX %>%
+  group_by(Timestamp) %>%
+  count()
+SurveyXLSX %>%
+  group_by(date(Timestamp)) %>%
+  count()
+
+##How many responses each week
+SurveyXLSX %>%
+  group_by(week(Timestamp)) %>%
+  count()
+
+
+#Factors
+##Most questions in this survey are factors - categorical responses in which the respondent selects 1 item
+##forcats package helps work with factors
+
+##First, change the columns to factors from character, use lapply
+colnames(SurveyXLSX) #get column names and index number to reference
+SurveyXLSX[,c(2:9)] <- lapply(SurveyXLSX[,c(2:9)], as.factor)
+
+#change order of factor levels: FCT_RELEVEL
+
+#change values of levels or collapse levels: FCT_RECODE or FCT_COLLAPSE
+fct_recode(SurveyXLSX$ExperienceProgCode, "minimal" = "Novice")
+fct_collapse(SurveyXLSX$ExperienceProgCode, Low = c("Basic knowledge or little to none", "Novice"))
+
+
+
+#Create new variables: MUTATE
+SurveyXLSX %>%
+  mutate(NewColumnExampleText = "manually add column data",
+         NewColumnExampleFormula = week(Timestamp))
+
+#Create new variables based upon criteria: CASE_WHEN and IFELSE
+
+##CASE_WHEN 2 examples. 1) more code, less complex Boolean operators 2) less code, more complex Boolean operators
+SurveyXLSX %>%
+  mutate(ExpRcat = case_when(ExperienceR=="Novice" ~ "Low",
+                             ExperienceR=="Basic knowledge or little to none" ~ "Low",
+                             TRUE ~ as.character(NA)))
+SurveyXLSX %>%
+  mutate(ExpRcat = case_when(ExperienceR=="Novice"|ExperienceR=="Basic knowledge or little to none" ~ "Low",
+                             TRUE ~ as.character(NA)))
+
+##IFELSE 2 examples. 1) more code, less complex Boolean operators 2) less code, more complex Boolean operators
+SurveyXLSX %>%
+  mutate(ExpRcat = if_else(ExperienceR=="Novice","Low",
+                         #  "other",
+                           if_else(ExperienceR=="Basic knowledge or little to none","Low","other"))
+         )
+SurveyXLSX %>%
+  mutate(ExpRcat = if_else(ExperienceR=="Novice"|ExperienceR=="Basic knowledge or little to none","Low","other"))
